7a514efae112f432845f0cd8f964c986e6eb0423
Welinghtonn <126928676+Welinghtonn@users.noreply.github.com>
2023-08-31 18:27:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function exercicio1(){
+    var nota = parseInt(prompt("Digite um numero"));
+
+    while(nota < 0 || nota > 10){
+        nota = parseInt(prompt("Digite novamente a nota"));
+    }
+
+    document.getElementById("root").style.display = "block"
+    document.getElementById("root").innerHTML = `
+        <p>A sua nota: ${nota}</p>
+    `
+}
+
+function exercicio2(){
+    var numero = 10
+
+    while(numero > 0){
+        
+    }
+}
+
+function limpar(){
+    document.getElementById("root").innerHTML = `
+        <p></p>
+    `
+}
