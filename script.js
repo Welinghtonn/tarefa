f1004215703ddc555a94777f0220db5a66606530
Welinghtonn <126928676+Welinghtonn@users.noreply.github.com>
2023-09-06 18:30:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 
-function exercicio1(){
+function exercicio1() {
     var nota = parseInt(prompt("Digite um numero"));
 
-    while(nota < 0 || nota > 10){
+    while (nota < 0 || nota > 10) {
         nota = parseInt(prompt("Digite novamente a nota"));
     }
-
     document.getElementById("root").style.display = "block"
-    document.getElementById("root").innerHTML = `
-        <p>A sua nota: ${nota}</p>
-    `
+    document.getElementById("root").innerHTML = `<p>A sua nota: ${nota}</p>`;
 }
+
 function exercicio2() {
     var numero = 10;
 
-    while (numero > 0) {
-        numero --;
-        document.getElementById("root").style.display = "block";
-        document.getElementById("root").innerHTML = `<p>Time: ${numero}</p>`;
+    const intervalo = setInterval(() => {
+        numero = numero - 1
+        document.getElementById("root").style.display = "block"
+        document.getElementById("root").innerHTML = `<p>A sua nota: ${numero}</p>`;
+
+        if (numero === 0) {
+            fecharintervalo();
+        }
+    }, 1000)
+
+
+    function fecharintervalo(){
+        clearInterval(intervalo)
+        
     }
 }
 
-function limpar(){
+
+function limpar() {
     document.getElementById("root").innerHTML = `
         <p></p>
     `
 }
+
